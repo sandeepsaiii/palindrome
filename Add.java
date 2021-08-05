cddbabaaa9d6cfc00315a3078789b0c6d06c3856
sandeepsaiii <87752070+sandeepsaiii@users.noreply.github.com>
2021-08-05 22:27:04 +0530

    program from eclipse

---
+++ Add.java
@@ -0,0 +1,20 @@
+package myfirstpackage;
+
+public class Calci {
+
+	public static void main(String[] args) {
+		Calci sai=new Calci();
+		sai.add();
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void add() {
+		int x=10;
+		int y=80;
+		int z=x+y;
+		System.out.println("The addition of two numbers"+z);
+		
+	}
+	
+}
